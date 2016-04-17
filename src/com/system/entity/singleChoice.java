3f6d3cf77b330086718a9ee5fb7baefb27160a33
zcp <1406261009@qq.com>
2016-04-17 14:43:08 +0800

    添加查询绑定老师

---
+++ src/com/system/entity/singleChoice.java
@@ -0,0 +1,55 @@
+package com.system.entity;
+
+public class singleChoice {
+	private String title;
+	private String choiceA;
+	private String choiceB;
+	private String choiceC;
+	private String ChoiceD;
+	private String correctAnswer;
+	private String answerAnalyze;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getChoiceA() {
+		return choiceA;
+	}
+	public void setChoiceA(String choiceA) {
+		this.choiceA = choiceA;
+	}
+	public String getChoiceB() {
+		return choiceB;
+	}
+	public void setChoiceB(String choiceB) {
+		this.choiceB = choiceB;
+	}
+	public String getChoiceC() {
+		return choiceC;
+	}
+	public void setChoiceC(String choiceC) {
+		this.choiceC = choiceC;
+	}
+	public String getChoiceD() {
+		return ChoiceD;
+	}
+	public void setChoiceD(String choiceD) {
+		ChoiceD = choiceD;
+	}
+	public String getCorrectAnswer() {
+		return correctAnswer;
+	}
+	public void setCorrectAnswer(String correctAnswer) {
+		this.correctAnswer = correctAnswer;
+	}
+	public String getAnswerAnalyze() {
+		return answerAnalyze;
+	}
+	public void setAnswerAnalyze(String answerAnalyze) {
+		this.answerAnalyze = answerAnalyze;
+	}
+	
+
+}
---
+++ src/com/system/service/viewTeacherService.java
@@ -0,0 +1,28 @@
+package com.system.service;
+import java.util.*;
+
+import com.system.entity.*;
+public class viewTeacherService {
+
+	public List<Teacher>getBindingTeachers(Student s){
+		List<Teacher> teachers = new ArrayList<Teacher>();
+		/*****************************
+		 * 
+		 * 获取已经绑定的教师具体实现
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		return teachers;
+		
+	}
+	
+	
+	
+}
