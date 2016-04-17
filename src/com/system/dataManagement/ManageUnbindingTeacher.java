3b2db68795b9f65ddd79a6a5045fd2454c3b4d83
yytwz96 <yytwz96@163.com>
2016-04-17 17:32:09 +0800

    wode xiugai

--- src/com/system/dataManagement/ManageUnbindingTeacher.java
+++ src/com/system/dataManagement/ManageUnbindingTeacher.java
@@ -1,19 +1,26 @@
 package com.system.dataManagement;
-import java.util.*;
+
+
+import java.util.Iterator;
+import java.util.List;
+
 import com.system.entity.Teacher;
+
 public class ManageUnbindingTeacher {
 
-	public List<Teacher>getUnbindingTeacher(List<Teacher> l1,List<Teacher> l2){
-		List<Teacher> teachers=l2;
-		for(Teacher t1:l1){
-			for(Teacher t2:l2){
-				if(t1.getEmail().equals(t2.getEmail())){
-					teachers.remove(t2);
+	public List<Teacher> getUnbindingTeacher(List<Teacher> l1, List<Teacher> l2) {
+		List<Teacher> tempList = l2;
+		Iterator<Teacher> itBinding = l1.iterator();
+		Iterator<Teacher> itAll = l2.iterator();
+		while (itAll.hasNext()) {
+			while (itBinding.hasNext()) {
+				Teacher t1 = itAll.next();
+				Teacher t2 = itBinding.next();
+				if (t1.getEmail().equals(t2.getEmail())) {
+					tempList.remove(t1);
 				}
 			}
-			
-		}	
-		return teachers;
-		
+		}
+		return tempList;
 	}
 }
