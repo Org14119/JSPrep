f6e99b91265582b45723b6dafe11f2a6a404af4b
zcp <1406261009@qq.com>
2016-04-17 17:30:48 +0800

    完成了添加绑定查重

--- src/com/system/dataManagement/ManageUnbindingTeacher.java
+++ src/com/system/dataManagement/ManageUnbindingTeacher.java
@@ -5,15 +5,14 @@ public class ManageUnbindingTeacher {
 
 	public List<Teacher>getUnbindingTeacher(List<Teacher> l1,List<Teacher> l2){
 		List<Teacher> teachers=l2;
-		for(Teacher t1:l1){
-			for(Teacher t2:l2){
-				if(t1.getEmail().equals(t2.getEmail())){
-					teachers.remove(t2);
+		for(int i=0;i<l1.size();i++){
+			for(int j=0;j<l2.size();j++){
+				if(l1.get(i).getEmail().equals(l2.get(j).getEmail())){
+					teachers.remove(j);
 				}
 			}
-			
-		}	
+		}
 		return teachers;
-		
+
 	}
 }
