b1b32111c600cebb6127a4e5e9cc308fc3c14a73
yytwz96 <yytwz96@163.com>
2016-04-20 19:31:19 +0800

    修改数据库插入题目的bug

--- src/com/system/daoImpl/ObjectQuestionDaoImpl.java
+++ src/com/system/daoImpl/ObjectQuestionDaoImpl.java
@@ -14,7 +14,7 @@ public class ObjectQuestionDaoImpl implements ObjectQuestionDAO {
 	@Override
 	public void insert(Connection conn, ObjectQuestion question, QuestionSpace space) throws SQLException {
 		// TODO Auto-generated method stub
-		String insertSQL = "INSERT INTO tbl_ObjectQuestion (questionContent,answer1,answer2,answer3,answer4,trueAnswer,analyze,score,questionSpaceID) VALUE(?,?,?,?,?,?,?,?,?)";
+		String insertSQL = "INSERT INTO tbl_ObjectQuestion (questionContent,answer1,answer2,answer3,answer4,trueAnswer,questionAnalyze,score,questionSpaceID) VALUES(?,?,?,?,?,?,?,?,?)";
 		PreparedStatement ps = conn.prepareStatement(insertSQL);
 		ps.setString(1, question.getTitle());
 		ps.setString(2, question.getChoiceA());
@@ -25,6 +25,7 @@ public class ObjectQuestionDaoImpl implements ObjectQuestionDAO {
 		ps.setString(7, question.getAnswerAnalyze());
 		ps.setInt(8, question.getScore());
 		ps.setLong(9, space.getId());
+		System.out.println(ps.toString());
 		ps.execute();
 
 	}
@@ -32,7 +33,7 @@ public class ObjectQuestionDaoImpl implements ObjectQuestionDAO {
 	@Override
 	public void update(Connection conn, ObjectQuestion question) throws SQLException {
 		// TODO Auto-generated method stub
-		String updateSQL = "UPDATE tbl_ObjectQuestion SET questionContent=?,answer1=?,answer2=?,answer3=?,answer4=?,trueAnswer=?,analyze=?,score=? WHERE questionID=?";
+		String updateSQL = "UPDATE tbl_ObjectQuestion SET questionContent=?,answer1=?,answer2=?,answer3=?,answer4=?,trueAnswer=?,questionAnalyze=?,score=? WHERE questionID=?";
 		PreparedStatement ps = conn.prepareStatement(updateSQL);
 		ps.setString(1, question.getTitle());
 		ps.setString(2, question.getChoiceA());
@@ -59,7 +60,7 @@ public class ObjectQuestionDaoImpl implements ObjectQuestionDAO {
 	public ResultSet get(Connection conn, QuestionSpace space) throws SQLException {
 		// TODO Auto-generated method stub
 
-		String getSQL = "SELECT tbl_objectquestion.questionID,tbl_objectquestion.questionContent ,tbl_objectquestion.answer1,tbl_objectquestion.answer2,tbl_objectquestion.answer3,tbl_objectquestion.answer4,tbl_objectquestion.trueAnswer,tbl_objectquestion.analyze,tbl_objectquestion.score FROM tbl_teacherquestionspace,tbl_objectquestion WHERE tbl_teacherquestionspace.id=? AND tbl_objectquestion.questionSpaceID=tbl_teacherquestionspace.id";
+		String getSQL = "SELECT tbl_objectquestion.questionID,tbl_objectquestion.questionContent ,tbl_objectquestion.answer1,tbl_objectquestion.answer2,tbl_objectquestion.answer3,tbl_objectquestion.answer4,tbl_objectquestion.trueAnswer,tbl_objectquestion.questionAnalyze,tbl_objectquestion.score FROM tbl_teacherquestionspace,tbl_objectquestion WHERE tbl_teacherquestionspace.id=? AND tbl_objectquestion.questionSpaceID=tbl_teacherquestionspace.id";
 		PreparedStatement ps = conn.prepareStatement(getSQL);
 		ps.setLong(1, space.getId());
 		return ps.executeQuery();
