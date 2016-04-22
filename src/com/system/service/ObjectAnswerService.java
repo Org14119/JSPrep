0df1b7b2503db8ecae5617b90e1e56a14e5e953c
yytwz96 <yytwz96@163.com>
2016-04-22 16:29:46 +0800

    添加通过testID查找考试answer的方法

--- src/com/system/service/ObjectAnswerService.java
+++ src/com/system/service/ObjectAnswerService.java
@@ -1,9 +1,10 @@
 package com.system.service;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import java.util.Map;
+import java.util.Vector;
 
 import com.system.daoImpl.ObjectAnswerInfoDaoImpl;
 import com.system.entity.ObjectAnswer;
@@ -12,6 +13,60 @@ import com.system.entity.Test;
 import com.system.util.ConnectionFactory;
 
 public class ObjectAnswerService {
+	/*
+	 * 
+	 * 通过Test的ID获取answer内容 传入Test对象，其中至少包含ID
+	 * 返回Vector<ObjectAnswer>,包含answer对象的ID，如果还要查看解析，需要调用其它方法; 如果发生异常，返回null
+	 * 
+	 */
+	public Vector<ObjectAnswer> getObjectAnswer(Test test) {
+		Connection conn = ConnectionFactory.getInstace().makeConnection();
+		ResultSet questionSet = null;
+		Vector<ObjectAnswer> questionV = new Vector<ObjectAnswer>();
+		try {
+			conn.setAutoCommit(false);
+			questionSet = new ObjectAnswerInfoDaoImpl().get(conn, test);
+			while (questionSet.next()) {
+				ObjectAnswer answer = new ObjectAnswer();
+				answer.setAnswerInfoID(questionSet.getLong("answerInfoID"));
+				answer.setAnswerContent(questionSet.getInt("studentAnswer"));
+				answer.setAnswerScore(questionSet.getInt("answerScore"));
+				answer.setChecked(false);
+				int tempState = questionSet.getInt("isChecked");
+				if (tempState == 1) {
+					answer.setChecked(true);
+				}
+				answer.setAnswerTime(questionSet.getTimestamp("answerTime").toString());
+				questionV.add(answer);
+			}
+			conn.commit();
+			return questionV;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return null;
+		} finally {
+			try {
+				questionSet.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
 	/*
 	 * 在学生回答了一道题之后使用。 传入test对象，其中包含ID属性 传入question对象，其中包含ID属性
 	 * 传入ObjectAnswer对象，其中包含学生的回答内容，回答的时间信息等 返回操作是否成功
